5aae79f970f3b4682675c06f6930d8cd37db2e55
Kaushal Diswar <86912351+StrangeKausu@users.noreply.github.com>
2021-07-12 20:51:20 +0530

    Update and rename °Fto°C.c.txt to ChangeTemp.c

--- ChangeTemp.c
+++ ChangeTemp.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    for (int i=0;i100;i++){
+    for (int i=0;i<100;i++){
     int a;
-    printf(Press 1 for Fahrenheit to 캜elcius otherwise Press 2 for 캜elcius to Fahrenheit  );
+    printf(Press 1 for Fahrenheit to 째Celcius otherwise Press 2 for 째Celcius to Fahrenheit  );
     scanf(%d,&a);
     
     if (a==1) {
@@ -12,7 +12,7 @@ int main()
         printf(Temperature in Fahrenheit  );
         scanf(%f,&Fahrenheit);
         Celcius=(Fahrenheit-32 )59 ;
-        printf(%.2f캜n,Celcius);
+        printf(%.2f째Cn,Celcius);
         printf(If you want to continue Press 5 otherwise Press 0 for exit  );
         scanf(%f,&j);
         if (j!=5){
@@ -21,10 +21,10 @@ int main()
     }
     else if (a==2) {
         float Fahrenheit,Celcius,k;
-        printf(Temperature in 캜elcius  );
+        printf(Temperature in 째Celcius  );
         scanf(%f,&Celcius);
         Fahrenheit=(9Celcius5) + 32;
-        printf(%.2f캟n,Fahrenheit);
+        printf(%.2f째Fn,Fahrenheit);
         printf(If you want to continue Press 5 otherwise Press 0 for exit  );
         scanf(%f,&k);
         if (k!=5){
