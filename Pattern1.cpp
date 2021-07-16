1524bae4d503be98c8479d7299f968ea02374a10
Kaushal Diswar <86912351+StrangeKausu@users.noreply.github.com>
2021-07-16 15:32:06 +0530

    Update Pattern1.cpp

--- Pattern1.cpp
+++ Pattern1.cpp
@@ -6,13 +6,11 @@ int main() {
     cout<<"Enter a number : ";
     cin>>a;
 	for (int i=1;i<=a;i++){
+	    if (i!=1){
+	    cout<<"\n";
+	    }
 	    for (int j=1;j<=i;j++){
-	        if (j==i){
-	        cout<<j<<"\n";
-	        }
-	        else{
-	            cout<<j;
-	        }
+	        cout<<j;
 	    }
 	}
 	return 0;
