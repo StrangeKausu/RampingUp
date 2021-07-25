43b1ad14283345330be48eed70fa437458a6b852
Kaushal Diswar <86912351+StrangeKausu@users.noreply.github.com>
2021-07-25 15:15:45 +0530

    Update TempConversion.c

--- TempConversion.c
+++ TempConversion.c
@@ -2,49 +2,44 @@
 
 int main()
 {
-    for (int i=0;i<100;i++){
-    int a;
-    printf("Press 1 for Fahrenheit to °Celcius  \n");
-    printf("Press 2 for °Celcius to Fahrenheit  \n");
-    scanf("%d",&a);
-    
-    if (a==1) {
-        float Fahrenheit,Celcius,j;
-        printf("Enter Temperature in Fahrenheit : ");
-        scanf("%f",&Fahrenheit);
+    float Fahrenheit,Celcius;
+    char ch;
+    do
+    {
+        printf ("Temperature Conversion Menu\n");
+        printf ("1.Fahrenheit to Celcius \n");
+        printf ("2.Celcius to Fahrenheit \n");
         
-        Celcius=(Fahrenheit-32) /1.8 ;
-        printf("The Temperature in Celcius is %.2f°C\n\n",Celcius);
-        printf("If you want to continue Press 5 otherwise Press 0 for exit : ");
-        scanf("%f",&j);
-        if (j!=5){
-            break;
-        }
-    }
-    else if (a==2) {
-        float Fahrenheit,Celcius,k;
-        printf("Enter Temperature in °Celcius : ");
-        scanf("%f",&Celcius);
+        int Choice;
+        printf ("Please Enter your Choice (1-2) : ");
+        scanf ("%d",&Choice);
         
-        Fahrenheit=(1.8*Celcius) + 32;
-        printf("The Temperature in Fahrenheit is %.2f°F\n\n",Fahrenheit);
-        printf("If you want to continue Press 5 otherwise Press 0 for exit : ");
-        scanf("%f",&k);
-        if (k!=5){
+        switch (Choice)
+        {
+            case 1:
+            printf("Enter Temperature in Fahrenheit : ");
+            scanf("%f",&Fahrenheit);
+            Celcius=(Fahrenheit-32) /1.8 ;
+            printf("The Temperature in Celcius is %.2f°C\n\n",Celcius);
             break;
-        }
-    }
-    else{
-        printf("YOU PRESS WRONG NUMBER\n\n");
-        int m;
-        printf("If you want to continue Press 5 otherwise Press 0 for exit : ");
-        scanf("%d",&m);
-        if (m!=5){
+            
+            case 2:
+            printf("Enter Temperature in Celcius : ");
+            scanf("%f",&Celcius);
+            Fahrenheit=(1.8*Celcius) + 32;
+            printf("The Temperature in Fahrenheit is %.2f°F\n\n",Fahrenheit);
             break;
+            
+            default:
+            printf ("WRONG CHOICE\n");
+            
         }
-    }
+        printf ("Want to continue (y/n)? : ");
+        scanf ("%s", &ch);
+        printf ("\n");
+        
+    } while (ch=='y' || ch=='Y');
     
-    }
     
     return 0;
 }
