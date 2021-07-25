1009003c9c905fa7c2741dd6674b353665dc5d00
Kaushal Diswar <86912351+StrangeKausu@users.noreply.github.com>
2021-07-25 14:42:25 +0530

    Update CalculateArea.py

--- CalculateArea.py
+++ CalculateArea.py
@@ -1,37 +1,46 @@
 for i in range(0,100):
-    a=int(input("Press 1 for Square Area , 2 for Rectangle Area , 3 for Triangle Area and 4 for Circle Area : "))
-    if a>4:
-        print("You Press Wrong No.")
-        break
 
-    elif a==1:
+    print ("Area Menu");
+    print ("1.Square");
+    print ("2.Rectangle");
+    print ("3.Triangle");
+    print ("4.Circle");
+    a=int(input("Please Enter your Choice (1-4) : "))
+    
+    if a==1:
         side=int(input("Enter a side of a Square : "))
         print(side*side)
-        x=int(input("If you want to continue Press 5 otherwise Press 0 to exit: "))
-        if x!=5:
+        ch=str(input("Want to continue (y/n)? : "))
+        if ch!="y" and ch!="Y":
             break
           
     elif a==2:
         length=int(input("Enter the length of the Rectangle : "))
         breadth=int(input("Enter the breadth of the Rectangle : "))
         print(length*breadth)
-        y=int(input("If you want to continue Press 5 otherwise Press 0 to exit: "))
-        if y!=5:
+        ch=str(input("Want to continue (y/n)? : "))
+        if ch!="y" and ch!="Y":
             break
        
     elif a==3:
         base=int(input("Enter the base of the Triangle : "))
         height=int(input("Enter the heigth of the Triangle : "))
         print(base*height/2)
-        z=int(input("If you want to continue Press 5 otherwise Press 0 to exit: "))
-        if z!=5:
+        ch=str(input("Want to continue (y/n)? : "))
+        if ch!="y" and ch!="Y":
             break
         
     elif a==4:
         radius=int(input("Enter the radius of the Circle : "))
         print((22*radius*radius)/7)
-        p=int(input("If you want to continue Press 5 otherwise Press 0 to exit: "))
-        if p!=5:
+        ch=str(input("Want to continue (y/n)? : "))
+        if ch!="y" and ch!="Y":
             break
-        
-        
+    
+    else:
+        print("WRONG CHOICE")
+        ch=str(input("Want to continue (y/n)? : "))
+        if ch!="y" and ch!="Y":
+            break
+     
+    
